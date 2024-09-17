1ed57e51cdf668a70f6424720ee40fbd76324bb2
hornyakjanos <68823455+hornyakjanos@users.noreply.github.com>
2024-09-17 11:08:11 +0200

    Add files via upload

---
+++ proba.py
@@ -0,0 +1,11 @@
+# Kérjük be az első számot a felhasználótól
+szam1 = float(input("Kérlek, adj meg az első számot: "))
+
+# Kérjük be a második számot a felhasználótól
+szam2 = float(input("Kérlek, adj meg a második számot: "))
+
+# Összeadjuk a két számot
+eredmeny = szam1 + szam2
+
+# Megjelenítjük az eredményt
+print("A két szám összege:", eredmeny)
